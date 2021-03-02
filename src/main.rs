1a4034e2dd3fc28c5149e4665972de724833134b
George Shuklin <george.shuklin@gmail.com>
2021-03-02 20:13:29 +0200

    Switch to streaming texture (with lock)

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,6 @@ use std::time::Duration;
 // const X:usize = 2560;
 // const Y:usize = 1440;
 
-fn noise(p: &mut u8, seed: u32, offset:u8){
-    if seed % 111 == 0 {
-        // *p = std::cmp::max(*p/2+*p/4, (seed>>offset) as u8 * (*p) + *p/2);
-        *p = (seed>>offset) as u8 ;
-    }
-}
 
 
 pub fn main() {
@@ -41,7 +35,7 @@ pub fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
     let texture_creator = canvas.texture_creator();
     let mut texture1 = texture_creator
-        .create_texture_target(sdl2::pixels::PixelFormatEnum::ABGR8888, X as u32, Y/cpus as u32)
+        .create_texture_streaming(sdl2::pixels::PixelFormatEnum::ABGR8888, X as u32, Y/cpus as u32)
         .unwrap();
     let mut pixels:Vec<u8> = Vec::new();
     pixels.resize((X*Y*4) as usize, 0);
@@ -50,45 +44,37 @@ pub fn main() {
     let mut factor:u32 = 0xFEFABABE + frames as u32;
     'running: loop {
         frames +=1;
-        if frames % 2 == 0 {
-            for y in 0..Y as usize{
-                for x in 0..X as usize{
-                    factor ^= factor << 13;
-                    factor ^= factor >> 17;
-                    factor ^= factor << 5;
-            
-                    noise(& mut pixels[(x+y*X as usize) *pixel_bytes], factor, 0);
-                    noise(& mut pixels[(x+y*X as usize) *pixel_bytes + 1], factor, 8);
-                    noise(& mut pixels[(x+y*X as usize) *pixel_bytes + 2], factor, 16);
-                    pixels[(x+y*X as usize) *4+3] = 255;
-                }
-            }
-        }
-        else{
-            for y in 0..Y as usize{
-                for x in 0..X as usize{
-                    let base = {
-
-                        if frames & 0x100 == 0{
-                            frames  as u8
-                        }
-                        else{
-                            0 - frames as u8
-                        }
-                    };
-                    pixels[(x+y*X as usize) *pixel_bytes] = base;
-                    pixels[(x+y*X as usize) *pixel_bytes+1] = base;
-                    pixels[(x+y*X as usize) *pixel_bytes+2] = base;
-                    pixels[(x+y*X as usize) *pixel_bytes+3] = 255;
-                }
-            }
-        }
-        
-        texture1.update(
+        texture1.with_lock(
             None,
-            &pixels,
-            X as usize,
+            |pixels, _pitch_size|{
+                if frames % 120 == 0 {
+                    for y in 0..Y as usize{
+                        for x in 0..X as usize{
+                            let base = {
+        
+                                if frames & 0x100 == 0{
+                                    frames  as u8
+                                }
+                                else{
+                                    0 - frames as u8
+                                }
+                            };
+                            pixels[(x+y*X as usize) *pixel_bytes] = base;
+                            pixels[(x+y*X as usize) *pixel_bytes+1] = base;
+                            pixels[(x+y*X as usize) *pixel_bytes+2] = base;
+                            pixels[(x+y*X as usize) *pixel_bytes+3] = 255;
+                        }
+                    }
+                }
+            }
         ).unwrap();
+            
+        
+        // texture1.update(
+        //     None,
+        //     &pixels,
+        //     X as usize,
+        // ).unwrap();
 
         canvas.copy(&texture1, None, None).unwrap();
         
